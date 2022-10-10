9340056c84a49f8b509917f086ad654299b2e8cb
Naji Kp <imnajikp@gmail.com>
2022-10-10 13:44:55 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Employee from "./Employee";
 function App() {
+
+  const emp=[
+    {name:'abdulla',age:22},
+    {name:'aslam',age:23},
+    {name:'badsha',age:21}
+  ];  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">{
+      emp.map((obj,index)=>{
+        return(
+          <Employee key={index} {...obj}/>
+        )
+      })
+    }
     </div>
   );
 }
